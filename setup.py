b4bc3ee7c66ae2319dbb55702d0554bf09fb9aa5
Antonio Rodrigues <antonio.projects.studio@gmail.com>
2024-11-07 16:01:24 +0300

    update dep

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         "PyYAML",
         "python-frontmatter",
-        "terminal_app @ git+https://github.com/antonio-projects-studio/terminal_app.git@53833aa3a4b043f377b1894135b2acdd33fdce0a",
+        "terminal_app @ git+https://github.com/antonio-projects-studio/terminal_app.git@75591667384f3789445c181935be56a6ba63105c",
         "markdown_reader @ git+https://github.com/antonio-projects-studio/markdown_reader.git@b8cec46c192a68346f63fb8b5bd5efd7afc1be61",
     ],
 )
